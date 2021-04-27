8740cec6fecbdf397af0c758d219e69f41124f31
Erick Dayle Loon <daylecarlie@gmail.com>
2021-04-28 06:44:02 +0800

    'Ready'

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b4702c7daae69f6f4b99fb05f9734571'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7f86eda6-5232-4a50-935f-23e8e7629396'
+};
